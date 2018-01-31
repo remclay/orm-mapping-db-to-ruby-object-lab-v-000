dfbab11364882fd4174ae375f012db2a4391bddb
Remmy Clay <remmy.lauren@gmail.com>
2018-01-31 19:49:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,6 +38,22 @@ class Student
     end.first
   end
 
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9
+      SQL
+
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+      end
+  end
+
+
+
+  
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
