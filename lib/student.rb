4bbb98f347d431b4c0c953b45f787d772572bdd8
Remmy Clay <remmy.lauren@gmail.com>
2018-01-31 20:00:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,19 +62,6 @@ class Student
     end
   end
 
-  def self.first_student_in_grade_10
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE grade = 10
-      LIMIT 1
-      SQL
-
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end.first
-  end
-
   def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
       SELECT *
@@ -88,8 +75,32 @@ class Student
     end
   end
 
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT 1
+      SQL
 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 
+  def self.all_students_in_grade_X
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+      SQL
+
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  
 
   def save
     sql = <<-SQL
