6c23d49268be8a7e2cc86bcac7b1243386e053f7
Remmy Clay <remmy.lauren@gmail.com>
2018-01-31 19:59:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,9 +70,9 @@ class Student
       LIMIT 1
       SQL
 
-      DB[:conn].execute(sql).map do |row|
-        self.new_from_db(row)
-      end.first
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def self.first_X_students_in_grade_10(x)
